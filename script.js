d47e8229c74f7e977b2457d52cea339c859a14ab
JayantGoel001 <jgoel92@gmail.com>
2021-08-21 15:52:01 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const deg = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+setInterval(()=> {
+	let date = new Date();
+	let hh = date.getHours() * 5 * deg;
+	let mm = date.getMinutes() * deg;
+	let ss = date.getSeconds() * deg;
+
+	hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+	mn.style.transform = `rotateZ(${mm}deg)`;
+	sc.style.transform = `rotateZ(${ss}deg)`;
+});
